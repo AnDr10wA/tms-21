06a448840aadb852559420271075d85db20d0479
oem <andr10wa@gmail.com>
2021-07-27 15:58:12 +0300

    hw01

---
+++ Task_1/task_1_4.py
@@ -0,0 +1,10 @@
+import math
+from math import sqrt
+
+a = 3
+b = 10
+
+sr_arif = (a+b)/2
+sf_geom = sqrt(a*b)
+print(f"Среднее арифметичесоке равно: {sr_arif}")
+print(f"Среднее геометричесоке равно {sf_geom}")
---
+++ Task_1/task_1_5.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+a = 3
+b = 4
+
+c = sqrt(a**2 + b**2)
+s = a*b/2
+print(f"Гипотенуза равна {c}")
+print(f"Площадь треугольнка равна {s}")
